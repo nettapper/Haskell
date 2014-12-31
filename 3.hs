8d86f72a705dfed44959c40e202495b1550927ca
Conner D <connerdunn7399@gmail.com>
2014-12-31 11:23:22 -0700

    created an isPrime function

---
+++ 3.hs
@@ -0,0 +1,10 @@
+-- The prime factors of 13195 are 5, 7, 13 and 29.
+-- What is the largest prime factor of the number 600851475143?
+
+-- Integral because isPrime should only deal with whole numbers
+isPrime :: Integral a => a -> Bool
+isPrime a = if a <= 1 then False else  -- Take care of case less or equal to 1
+                if (length divEvenList) /= 0
+                then False
+                else True
+                    where divEvenList = [x | x <- [2..(a-1)], (mod a x) == 0]
