1c1847123d7fa2d756eee4b4a46b59567d83652c
Conner D <connerdunn7399@gmail.com>
2016-03-20 11:22:37 -0600

    Added comment for the first two functions

--- Upenn/HW1.hs
+++ Upenn/HW1.hs
@@ -2,11 +2,13 @@ import Data.Char
 
 -- Validate Credit Cards
 
+-- convert positive integers to list of digits
 toDigits :: Integer -> [Integer]
 toDigits n
     | n <= 0 = []
     | otherwise = map toInteger $ map digitToInt (show n)
 
+-- reverse toDigits
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n = reverse $ toDigits n
 
