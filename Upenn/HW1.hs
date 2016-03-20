86c3eb4b3076833f2e0eabd7713c2cbdaa80e20e
Conner D <connerdunn7399@gmail.com>
2016-03-20 00:04:01 -0600

    Built up a toDigitsRev function

--- Upenn/HW1.hs
+++ Upenn/HW1.hs
@@ -7,7 +7,9 @@ toDigits n
     | n <= 0 = []
     | otherwise = map toInteger $ map digitToInt (show n)
 
--- toDigitsRev :: Integer -> [Integer]
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = reverse $ toDigits n
+
 -- doubleEveryOther :: [Integer] -> [Integer]
 -- sumDigits :: [Integer] -> Integer
 -- validate :: Integer -> Bool
