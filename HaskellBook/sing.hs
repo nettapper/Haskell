022d7bcf78d126e80270c7d2e3420a78d5c4c55c
Conner D <connerdunn7399@gmail.com>
2016-11-05 12:05:32 -0600

    Sing is now singing a different song

--- HaskellBook/sing.hs
+++ HaskellBook/sing.hs
@@ -6,6 +6,6 @@ fstString x = x ++ " in the rain"
 sndString :: String -> String
 sndString x = x ++ " over the rainbow"
 
-sing = if x > y then fstString x else sndString y
+sing = if x < y then fstString x else sndString y
   where x = "Singin"
         y = "Somewhere"
