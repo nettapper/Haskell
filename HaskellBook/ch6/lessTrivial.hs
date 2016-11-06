bd1005003f3e279e75e2a09940b992fc072fb925
Conner D <connerdunn7399@gmail.com>
2016-11-06 01:29:08 -0600

    instance of Ord for my DayOfTheWeek

--- HaskellBook/ch6/lessTrivial.hs
+++ HaskellBook/ch6/lessTrivial.hs
@@ -5,12 +5,15 @@ module LessTrivial where
 
 data DayOfTheWeek =
   Mon | Tue | Weds | Thu | Fri | Sat | Sun
-  deriving Show
+  deriving (Show)
+  -- deriving (Eq)  -- I could also now use Eq rather than my own implementation
+  -- deriving (Ord)  -- when using ord Mon is less than Tue because Mon is on the left
 
 data Date =
   Date DayOfTheWeek Int
   deriving Show
 
+-- As a general note, I need to ensure that Eq and Ord make sense w/ respect to eachother!
 instance Eq DayOfTheWeek where
   (==) Mon Mon   = True
   (==) Tue Tue   = True
@@ -20,6 +23,12 @@ instance Eq DayOfTheWeek where
   (==) Sat Sat   = True
   (==) Sun Sun   = True
   (==) _ _       = False  -- Wall will complain if I leave this out
+
+instance Ord DayOfTheWeek where  -- I like Fridays
+  compare Fri Fri = EQ
+  compare Fri _   = GT
+  compare _ Fri   = LT
+  compare _ _     = EQ
 
 iHateMondays = Mon == Mon
 
