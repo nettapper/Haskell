721d45148c6ebd790e916f636ce4b90dc2aa4caf
Conner D <connerdunn7399@gmail.com>
2016-11-20 15:37:48 -0700

    Composibility is a cool thing Zip defn w/ ZipWith!

---
+++ HaskellBook/ch9/zipping.hs
@@ -0,0 +1,16 @@
+module Zipping where
+
+myZip :: [a] -> [b] -> [(a,b)]
+myZip [] _          = []
+myZip _ []          = []
+myZip (x:xs) (y:ys) = (x,y) : myZip xs ys
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith _ [] _          = []
+myZipWith _ _ []          = []
+myZipWith f (x:xs) (y:ys) = f x y : myZipWith f xs ys
+
+myZip2 :: [a] -> [b] -> [(a,b)]
+myZip2 [] _  = []
+myZip2 _ []  = []
+myZip2 xs ys = myZipWith (,) xs ys
