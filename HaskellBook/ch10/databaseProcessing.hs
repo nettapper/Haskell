769c3f30f7dad51de95d812d4018cd26ab22e476
Conner D <connerdunn7399@gmail.com>
2016-11-23 22:02:39 -0700

    I can filter times out of out 'database' -- not a db

---
+++ HaskellBook/ch10/databaseProcessing.hs
@@ -0,0 +1,42 @@
+import Data.Time
+
+data DatabaseItem =
+    DbString String
+    | DbNumber Integer
+    | DbDate UTCTime
+    deriving (Eq, Ord, Show)
+
+
+theDatabase :: [DatabaseItem]
+theDatabase =
+  [ DbDate (UTCTime
+           (fromGregorian 1911 5 1)
+           (secondsToDiffTime 34123))
+  , DbNumber 9001
+  , DbString "Hello, world!"
+  , DbDate (UTCTime
+           (fromGregorian 1921 5 1)
+           (secondsToDiffTime 34123))
+  ]
+
+filterDbDate :: [DatabaseItem] -> [UTCTime]
+filterDbDate = foldr onlyUTCTime []
+  where onlyUTCTime :: DatabaseItem -> [UTCTime] -> [UTCTime]
+        onlyUTCTime dbItem utcList = case dbItem of
+                                          (DbString _) -> utcList
+                                          (DbNumber _) -> utcList
+                                          (DbDate utc) -> [utc] ++ utcList
+
+filterDbNumber :: [DatabaseItem] -> [Integer]
+filterDbNumber = undefined
+
+mostRecent :: [DatabaseItem] -> UTCTime
+mostRecent = undefined
+
+sumDb :: [DatabaseItem] -> Integer
+sumDb = undefined
+
+-- You'll probably need to use fromIntegral
+-- to get from Integer to Double.
+avgDb :: [DatabaseItem] -> Double
+avgDb = undefined
