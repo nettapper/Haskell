efb5666a75046993a505e50e3eb886d5c20858c0
Conner D <connerdunn7399@gmail.com>
2016-11-26 18:32:56 -0700

    Whoops some silly mistakes, now is working

--- HaskellBook/ch10/myReverse.hs
+++ HaskellBook/ch10/myReverse.hs
@@ -1,9 +1,12 @@
 -- just def'ning my own rev funcs
 
 -- foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b
-revl = foldl (\a b -> b ++ [a]) []
+revl :: Foldable t => t a -> [a]
+-- revl = foldl (\b a -> [a] ++ b) []
+revl = foldl (flip (:)) []
 
 -- foldr :: Foldable t => (a -> b -> b) -> b -> t a -> b
+revr :: Foldable t => t a -> [a]
 revr = foldr (\a b -> b ++ [a]) []
 
 -- the difference between foldl and foldr is apparent
