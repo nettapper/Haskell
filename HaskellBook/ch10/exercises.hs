719268a22b685e5fa69259bb28cf291a5fac28cd
Conner D <connerdunn7399@gmail.com>
2016-11-27 23:42:47 -0700

    Done q1 of warmup and review!

---
+++ HaskellBook/ch10/exercises.hs
@@ -0,0 +1,20 @@
+stops  = "pbtdkg"
+vowels = "aeiou"
+
+threeTupleMaker :: String -> String -> [(Char, Char, Char)]
+threeTupleMaker stops vowels = permThree stops vowels stops
+  where permThree :: String -> String -> String -> [(Char, Char, Char)]
+        permThree xs ys zs = [(x, y, z) | x <- xs, y <- ys, z <- zs]
+
+threeTupleMaker' :: [(Char, Char, Char)]
+threeTupleMaker' = filter firstMustHaveP $ threeTupleMaker stops vowels
+  where firstMustHaveP :: (Char, Char, Char) -> Bool
+        firstMustHaveP (c, _, _) = c == 'p'
+
+nouns = ["dog", "cat"]
+verbs = ["run", "eat", "fly"]
+
+threeTupleMaker'' :: [a] -> [a] -> [(a, a, a)]
+threeTupleMaker'' stops vowels = permThree stops vowels stops
+  where permThree :: [a] -> [a] -> [a] -> [(a, a, a)]
+        permThree xs ys zs = [(x, y, z) | x <- xs, y <- ys, z <- zs]
