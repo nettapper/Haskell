f44f007b25c53403f15a5550f53b5c173f296b01
Conner D <connerdunn7399@gmail.com>
2016-12-02 11:24:47 -0700

    Showing the types of my data constructors

--- HaskellBook/ch11/playingWithData.hs
+++ HaskellBook/ch11/playingWithData.hs
@@ -1,9 +1,15 @@
 -- data MyList = Null | Cons a MyList  -- Not in scope type var 'a'
 -- data MyList a = Null | Cons a MyList  -- Don't forget to pass 'a' to the type constructor
-data MyList a = Null | Cons a (MyList a)
+-- data MyList a = Null | Cons a (MyList a)
+data MyList hi = Null | Cons hi (MyList hi)  -- remember that names of vars don't matter
   deriving Show
 
 -- To see the kind of MyList
 -- :k MyList
 -- MyList * -> *
 
+-- :t Null
+-- Null :: MyList hi
+
+-- :t Cons
+-- Cons :: hi -> MyList hi -> MyList hi
