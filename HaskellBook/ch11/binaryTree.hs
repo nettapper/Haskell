2a1207393176bba48ffa73036376990a7413b2bd
Conner D <connerdunn7399@gmail.com>
2016-12-24 17:23:12 -0700

    defs for pre / in / post -order of binaryTrees

--- HaskellBook/ch11/binaryTree.hs
+++ HaskellBook/ch11/binaryTree.hs
@@ -27,3 +27,48 @@ mapOkay =
   if mapTree (+1) testTree' == mapExpected
   then print "yup okay!"
   else error "test failed!"
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder tree = preorder' tree []
+  where preorder' Leaf list                = list
+        preorder' (Node left a right) list = [a] ++ (preorder' left list) ++ (preorder' right list)
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder tree = inorder' tree []
+  where inorder' Leaf list                = list
+        inorder' (Node left a right) list = (inorder' left list) ++ [a] ++ (inorder' right list)
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder tree = postorder' tree []
+  where postorder' Leaf list                = list
+        postorder' (Node left a right) list =(postorder' left list) ++ (postorder' right list) ++ [a]
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder =
+  if preorder testTree == [2, 1, 3]
+  then putStrLn "Preorder fine!"
+  else putStrLn "Bad news bears."
+
+testInorder :: IO ()
+testInorder =
+  if inorder testTree == [1, 2, 3]
+  then putStrLn "Inorder fine!"
+  else putStrLn "Bad news bears."
+
+testPostorder :: IO ()
+testPostorder =
+  if postorder testTree == [1, 3, 2]
+  then putStrLn "Postorder fine!"
+  else putStrLn "postorder failed check"
+
+main :: IO ()
+main = do
+  testPreorder
+  testInorder
+  testPostorder
