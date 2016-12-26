31b0f3bfac7c89d100d1dd7769dced420a21f0f7
Conner D <connerdunn7399@gmail.com>
2016-12-25 22:31:53 -0700

    I can now encode with my vigenre cypher

--- HaskellBook/ch11/vigenre.hs
+++ HaskellBook/ch11/vigenre.hs
@@ -1,17 +1,32 @@
-keyword :: String
-keyword = "ally"
+import Data.Char
 
-message :: String
-message = "meet at dawn"
+myKeyword :: String
+myKeyword = "ally"
+
+myMessage :: String
+myMessage = "meet at dawn"
+
+caesar :: Int -> Char -> Char
+caesar _ ' ' = ' '
+caesar numShiftRight val = denormalize (newVal val numShiftRight)
+  where denormalize n = chr $ n + ord 'a'
+        normalize c = ord c - ord 'a'
+        newVal c shift = mod (normalize c + shift) 26
 
 encode :: String -> String -> String
-encode m k = undefined
+encode message keyword = map f $ myZip message (cycle keyword)
+  where myZip [] _ = []
+        myZip (x:xs) yss@(y:ys) = if x == ' '
+                              then (x, 'a') : myZip xs yss
+                              else (x, y) : myZip xs ys
+        f (letter, keyletter) = caesar (numShift keyletter) letter
+        numShift k = mod (ord k - ord 'a') 26
 
 decode :: String -> String -> String
-decode m k = undefined
+decode message keyword = undefined
 
 test :: IO ()
-test = if decode (encode message keyword) keyword == message
+test = if decode (encode myMessage myKeyword) myKeyword == myMessage
        then putStrLn "it is working"
        else putStrLn "messages don't match"
 
