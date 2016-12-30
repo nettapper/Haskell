9e0a62c992039f9c651d80ebbf4efdfe042a829b
Conner D <connerdunn7399@gmail.com>
2016-12-30 00:06:04 -0700

    Ok, so this now uses my capitalizeWord paragraph

--- HaskellBook/ch11/asPatterns.hs
+++ HaskellBook/ch11/asPatterns.hs
@@ -15,10 +15,22 @@ capitalizeWord []     = []
 capitalizeWord (x:xs) = toUpper x : xs
 
 capitalizeParagraph :: String -> String
-capitalizeParagraph []     = []
-capitalizeParagraph (x:xs) = if x == '.'
-                             then x : capNextLet xs
-                             else x : capitalizeParagraph xs
-                               where capNextLet []       = []
-                                     capNextLet (' ':xs) = ' ' : capNextLet xs
-                                     capNextLet (x:xs)   = toUpper x : capitalizeParagraph xs
+capitalizeParagraph [] = []
+capitalizeParagraph xs = unwords $ capNextWord $ words xs
+
+capNextWord []       = []
+capNextWord [x]      = [x]
+capNextWord (x:y:xs) = if null x
+                          then x : capNextWord (y:xs)
+                          else if last x == '.'
+                            then x : capitalizeWord y : capNextWord xs
+                            else x : capNextWord (y:xs)
+
+capitalizeParagraph' :: String -> String
+capitalizeParagraph' []     = []
+capitalizeParagraph' (x:xs) = if x == '.'
+                                 then x : capNextLet xs
+                                 else x : capitalizeParagraph xs
+                                   where capNextLet []       = []
+                                         capNextLet (' ':xs) = ' ' : capNextLet xs
+                                         capNextLet (x:xs)   = toUpper x : capitalizeParagraph xs
