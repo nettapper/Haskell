b37fe743d528f3d75122ac53a394bd8d1b8994d8
Conner D <connerdunn7399@gmail.com>
2016-12-29 23:49:36 -0700

    This is an ok solution if I didn't read the instructions

--- HaskellBook/ch11/asPatterns.hs
+++ HaskellBook/ch11/asPatterns.hs
@@ -1,4 +1,4 @@
-import Data.Char
+import Data.Char (toUpper)
 
 isSubsequence :: (Eq a) => [a] -> [a] -> Bool
 isSubsequence [] _      = True
@@ -10,3 +10,15 @@ capitalizeWords sentence = foldr capitalize [] $ words sentence
   where capitalize [] b       = b
         capitalize word@(a:as) b = (word, toUpper a : as) : b
 
+capitalizeWord :: String -> String
+capitalizeWord []     = []
+capitalizeWord (x:xs) = toUpper x : xs
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph []     = []
+capitalizeParagraph (x:xs) = if x == '.'
+                             then x : capNextLet xs
+                             else x : capitalizeParagraph xs
+                               where capNextLet []       = []
+                                     capNextLet (' ':xs) = ' ' : capNextLet xs
+                                     capNextLet (x:xs)   = toUpper x : capitalizeParagraph xs
