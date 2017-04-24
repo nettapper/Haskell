5b53716be0b00d3a426efad4b022943e861b480f
Conner D <connerdunn7399@gmail.com>
2017-04-24 11:22:09 -0600

    Finished huttonsRazor, and finished ch11 :hamburger:

--- HaskellBook/ch11/huttonsRazor.hs
+++ HaskellBook/ch11/huttonsRazor.hs
@@ -5,9 +5,11 @@ data Expr
 -- eval (Add (Lit 1) (Lit 9001))
 -- 9002
 eval :: Expr -> Integer
-eval = error "todo"
+eval (Lit z) = z
+eval (Add exprA exprB) = eval exprA + eval exprB
 
 -- printExpr (Add (Lit 1) (Lit 9001))
 -- "1 + 9001"
 printExpr :: Expr -> String
-printExpr = undefined
+printExpr (Lit z) = show z
+printExpr (Add exprA exprB) = printExpr exprA ++ " + " ++ printExpr exprB
