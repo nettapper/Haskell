472ef2f41b94f92befabc55d7d7e28209f10b1ac
Conner D <connerdunn7399@gmail.com>
2017-06-19 13:31:15 -0600

    Finished the count the before a vowel function

--- HaskellBook/ch12/stringProcessing.hs
+++ HaskellBook/ch12/stringProcessing.hs
@@ -15,3 +15,25 @@ replaceThe :: String -> String
 replaceThe s = unwords $ map nothingToA $ map notThe $ words s
   where nothingToA (Just s)  = s
         nothingToA Nothing = "a"
+
+-- >>> countTheBeforeVowel "the cow"
+-- 0
+-- >>> countTheBeforeVowel "the evil cow"
+-- 1
+-- countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel s = sum $ map f zs
+    where ws = words s
+          zs = zip (map notThe ws) (tail ws)
+          f (a,b) = case a of
+                         Nothing -> if startsWithVowel b then 1 else 0
+                         otherwise -> 0
+
+startsWithVowel :: String -> Bool
+startsWithVowel (s:_) = case s of
+                             'a' -> True
+                             'e' -> True
+                             'i' -> True
+                             'o' -> True
+                             'u' -> True
+                             otherwise -> False
+
