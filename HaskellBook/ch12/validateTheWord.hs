bab70d60b926938cf432107bbd33bd0354df6fa2
Conner D <connerdunn7399@gmail.com>
2017-06-23 13:18:26 -0600

    No longer using intersect from Data.List

--- HaskellBook/ch12/validateTheWord.hs
+++ HaskellBook/ch12/validateTheWord.hs
@@ -12,11 +12,24 @@ mkWord s = if v > c then Nothing else Just (Word' s)
   where v = numVowels s
         c = numConsonants s
 
--- I'm not sure if it's cheating to use intersect from Data.List
 numVowels :: String -> Integer
-numVowels = toInteger . length . flip intersect vowels
+numVowels = toInteger . length . flip myIntersect vowels
+
+-- I'm not sure if it's cheating to use intersect from Data.List
+numVowels' :: String -> Integer
+numVowels' = toInteger . length . flip intersect vowels
 
 numConsonants :: String -> Integer
 numConsonants s = toInteger total - numVowels s
   where total = length s
 
+myIntersect :: Eq a => [a] -> [a] -> [a]
+myIntersect xs ys = [x | x <- xs, x `isIn` ys]
+
+isIn :: Eq a => a -> [a] -> Bool
+isIn _ [] = False
+isIn x (y:ys) = x == y || isIn x ys
+
+-- Needed to make GhcMod work!
+main :: IO()
+main = return ()
