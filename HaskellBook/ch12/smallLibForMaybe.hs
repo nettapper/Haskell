4d8425a05d187ebdd053268099d95cba36e065c1
Conner D <connerdunn7399@gmail.com>
2017-06-24 15:37:40 -0600

    Not very efficient but ¯\_(ツ)_/¯

--- HaskellBook/ch12/smallLibForMaybe.hs
+++ HaskellBook/ch12/smallLibForMaybe.hs
@@ -15,6 +15,59 @@ isNothing :: Maybe a -> Bool
 isNothing = not . isJust
 
 
+-- >>> mayybee 0 (+1) Nothing
+--0
+-- >>> mayybee 0 (+1) (Just 1)
+--2
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee b _ Nothing  = b
+mayybee _ f (Just a) = f a
+
+-- >>> fromMaybe 0 Nothing
+-- 0
+-- >>> fromMaybe 0 (Just 1)
+-- 1
+fromMaybe :: a -> Maybe a -> a
+fromMaybe a Nothing  = a
+fromMaybe _ (Just a) = a
+
+-- >>> listToMaybe [1, 2, 3]
+-- Just 1
+-- >>> listToMaybe []
+-- Nothing
+listToMaybe :: [a] -> Maybe a
+listToMaybe []    = Nothing
+listToMaybe (x:_) = Just x
+
+-- >>> maybeToList (Just 1)
+-- [1]
+-- >>> maybeToList Nothing
+-- []
+maybeToList :: Maybe a -> [a]
+maybeToList = mayybee [] (:[])
+
+
+-- >>> catMaybes [Just 1, Nothing, Just 2]
+-- [1, 2]
+-- >>> catMaybes [Nothing, Nothing, Nothing]
+-- []
+catMaybes :: [Maybe a] -> [a]
+catMaybes = foldl f []
+  where f xs Nothing = xs
+        f xs (Just a) = xs ++ [a]
+
+
+-- >>> flipMaybe [Just 1, Just 2, Just 3]
+-- Just [1, 2, 3]
+-- >>> flipMaybe [Just 1, Nothing, Just 3]
+-- Nothing
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe xs = if length xs == length ys
+                  then Just ys
+                  else Nothing
+                    where ys = catMaybes xs
+-- The above isn't very efficient, but ¯\_(ツ)_/¯
+
 -- Needed to make GhcMod work!
 main :: IO()
 main = return ()
