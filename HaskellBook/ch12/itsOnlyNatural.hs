21e13373a95dd9636fd381bb77e471042694e845
Conner D <connerdunn7399@gmail.com>
2017-06-24 15:10:48 -0600

    init commit of itsOnlyNatural

---
+++ HaskellBook/ch12/itsOnlyNatural.hs
@@ -0,0 +1,29 @@
+-- As natural as any competitive bodybuilder
+data Nat =
+    Zero
+  | Succ Nat
+  deriving (Eq, Show)
+
+-- >>> natToInteger Zero
+-- 0
+-- >>> natToInteger (Succ Zero)
+-- 1
+-- >>> natToInteger (Succ (Succ Zero))
+-- 2
+natToInteger :: Nat -> Integer
+natToInteger = undefined
+
+-- >>> integerToNat 0
+-- Just Zero
+-- >>> integerToNat 1
+-- Just (Succ Zero)
+-- >>> integerToNat 2
+-- Just (Succ (Succ Zero))
+-- >>> integerToNat (-1)
+-- Nothing
+integerToNat :: Integer -> Maybe Nat
+integerToNat = undefined
+
+-- Needed to make GhcMod work!
+main :: IO()
+main = return ()
