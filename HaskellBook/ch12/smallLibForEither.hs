cfe0223b0befc4c61fa33f92fe60dd718cb21a4e
Conner D <connerdunn7399@gmail.com>
2017-06-25 13:40:19 -0600

    Checked against the defn in Data.Either :checkered_flag:

--- HaskellBook/ch12/smallLibForEither.hs
+++ HaskellBook/ch12/smallLibForEither.hs
@@ -10,6 +10,22 @@ rights' = foldr f []
   where f (Right r) list = r : list
         f (Left _) list = list
 
+partitionEithers' :: [Either a b] -> ([a], [b])
+partitionEithers' es = (lefts' es, rights' es)
+
+eitherMaybe' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe' f (Right r) = Just $ f r
+eitherMaybe' _ (Left _) = Nothing
+
+either'::(a -> c) -> (b -> c) -> Either a b -> c
+either' fac fbc (Left a) = fac a
+either' fac fbc (Right b) = fbc b
+
+eitherMaybe'' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe'' _ (Left _) = Nothing
+eitherMaybe'' fbc (Right b) = Just $ fbc b
+
+
 -- Needed to make GhcMod work!
 main :: IO()
 main = return ()
