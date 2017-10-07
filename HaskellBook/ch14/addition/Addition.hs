d7f31005367bafbf626c7ef134e5ec811e012c1b
Conner D <connerdunn7399@gmail.com>
2017-10-06 18:32:41 -0600

    Testing another one from the past, myMult (mult from sums)

--- HaskellBook/ch14/addition/Addition.hs
+++ HaskellBook/ch14/addition/Addition.hs
@@ -18,6 +18,13 @@ main = hspec $ do
     it "22 dividedBy 5 is 4 remainder 2" $ do
       dividedBy 22 5 `shouldBe` (4, 2)
 
+    it "15 myMult 5 is 75" $ do
+      15 `myMult` 5 `shouldBe` (75 :: Integer)
+    it "1 myMult 432434 is 432434" $ do
+      1 `myMult` 432434 `shouldBe` (432434 :: Integer)
+    it "0 myMult 10 is 0" $ do
+      0 `myMult` 10 `shouldBe` (0 :: Integer)
+
 
 
 -- A blast from the past, something from recursion in ch08!
@@ -28,3 +35,9 @@ dividedBy num dem = go num dem 0
           | otherwise = go (n - d) d (count + 1)
 
 
+-- More of a blast from the past, something else from recursion in ch08!
+-- multiplication by sum
+myMult :: (Integral a) => a -> a -> a
+myMult 1 y = y
+myMult x 1 = x
+myMult x y = myMult x (y - 1) + x
