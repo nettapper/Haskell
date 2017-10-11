401684452f15de64bb65ffc524ac1a41121b95bf
Conner D <connerdunn7399@gmail.com>
2017-10-11 14:10:35 -0600

    Cool! I'm testing my char conversion!!

--- HaskellBook/ch14/morse/tests/tests.hs
+++ HaskellBook/ch14/morse/tests/tests.hs
@@ -1,4 +1,27 @@
-module Tests where
+module Main where
+
+import qualified Data.Map as M
+import Morse
+import Test.QuickCheck
+
+allowedChars :: [Char]
+allowedChars = M.keys letterToMorse
+
+allowedMorse :: [Morse]
+allowedMorse = M.elems letterToMorse
+
+charGen :: Gen Char
+charGen = elements allowedChars
+
+morseGen :: Gen Morse
+morseGen = elements allowedMorse
+
+prop_thereAndBackAgain :: Property
+prop_thereAndBackAgain =
+  forAll charGen
+  (\c -> ((charToMorse c)
+    >>= morseToChar) == Just c)
+
 
 main :: IO ()
-main = putStrLn "hello"
+main = quickCheck prop_thereAndBackAgain
