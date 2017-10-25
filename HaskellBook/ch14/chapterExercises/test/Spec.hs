3e93b89e1ffee93f03e6c2540ef53e0d6aaf92ab
Conner D <connerdunn7399@gmail.com>
2017-10-24 19:38:42 -0600

    List ordered property

--- HaskellBook/ch14/chapterExercises/test/Spec.hs
+++ HaskellBook/ch14/chapterExercises/test/Spec.hs
@@ -1,5 +1,6 @@
 import TestWordNumber (run)
 import Test.QuickCheck
+import Data.List (sort)
 
 main :: IO ()
 main = do
@@ -11,7 +12,17 @@ main = do
 usingQC :: IO ()
 usingQC = do
   quickCheck $ \x -> x == halfIdentity (x :: Double)
+  quickCheck $ ((listOrdered . sort) :: [Integer] -> Bool)
+  quickCheck $ ((listOrdered . sort) :: [String] -> Bool)
+  quickCheck $ ((listOrdered . sort) :: [Int] -> Bool)
+
+
+-- for any sorted list this should hold
+listOrdered :: (Ord a) => [a] -> Bool
+listOrdered xs = snd $ foldr go (Nothing, True) xs
+ where go _ status@(_, False) = status
+       go y (Nothing, t) = (Just y, t)
+       go y (Just x, t) = (Just y, x >= y)
 
 half x = x / 2
-
 halfIdentity = (*2) . half
