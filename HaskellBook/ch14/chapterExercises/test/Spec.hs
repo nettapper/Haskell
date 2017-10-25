9b3d207dbe641faeeadcb1f85f122ed192b1bb04
Conner D <connerdunn7399@gmail.com>
2017-10-24 19:32:18 -0600

    quickCheck #1 done

--- HaskellBook/ch14/chapterExercises/test/Spec.hs
+++ HaskellBook/ch14/chapterExercises/test/Spec.hs
@@ -1,4 +1,17 @@
 import TestWordNumber (run)
+import Test.QuickCheck
 
 main :: IO ()
-main = run
+main = do
+  putStrLn "TestWordNumber"
+  run
+  putStrLn "--------------"
+  usingQC
+
+usingQC :: IO ()
+usingQC = do
+  quickCheck $ \x -> x == halfIdentity (x :: Double)
+
+half x = x / 2
+
+halfIdentity = (*2) . half
