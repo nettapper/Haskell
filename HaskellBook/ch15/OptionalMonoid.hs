13466d283711cdfa4ed442b08a2bcedfe86f9d09
Conner D <connerdunn7399@gmail.com>
2017-11-05 19:51:45 -0700

    Finised the OptionalMonoid exercise

---
+++ HaskellBook/ch15/OptionalMonoid.hs
@@ -0,0 +1,24 @@
+module OptionalMonoid where
+
+import Data.Monoid ((<>), Sum(..), Product(..))
+
+data Optional a =
+    Nada
+  | Only a
+  deriving (Eq, Show)
+
+instance Monoid a
+      => Monoid (Optional a) where
+  mempty = Nada
+  mappend Nada Nada = mempty
+  mappend Nada (Only a) = Only (a <> mempty)
+  mappend (Only a) Nada = Only (a <> mempty)
+  mappend (Only a) (Only b) = Only (a <> b)
+
+main :: IO ()
+main = do
+  putStrLn $ show $ Only (Sum 1) `mappend` Only (Sum 1)
+  putStrLn $ show $ Only (Product 4) `mappend` Only (Product 2)
+  putStrLn $ show $ Only (Sum 1) `mappend` Nada
+  putStrLn $ show $ Only [1] `mappend` Nada
+  putStrLn $ show $ Nada `mappend` Only (Sum 1)
