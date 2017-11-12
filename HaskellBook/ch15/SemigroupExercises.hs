5775e86c9b8c65ca7a98af02a72014d5fc56bd55
Conner D <connerdunn7399@gmail.com>
2017-11-12 11:36:45 -0700

    Just two more questions of SemigroupExercises

--- HaskellBook/ch15/SemigroupExercises.hs
+++ HaskellBook/ch15/SemigroupExercises.hs
@@ -32,8 +32,39 @@ instance (Arbitrary a) => Arbitrary (Identity a) where
 
 type IdentityAssc a = (Identity a) -> (Identity a) -> (Identity a) -> Bool
 -- End Question 2
+-- Question 3
+data Two a b = Two a b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two a b) <> (Two a' b') = Two (a <> a') (b <> b')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return (Two a b)
+
+type TwoAssc a b = (Two a b) -> (Two a b) -> (Two a b) -> Bool
+-- End Question 3
+-- Question 4
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c) => Semigroup (Three a b c) where
+  (Three a b c) <> (Three a' b' c') = Three (a <> a') (b <> b') (c <> c')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (Three a b c)
+
+type ThreeAssc a b c = Three a b c -> Three a b c -> Three a b c -> Bool
+-- End Question 4
 
 main :: IO ()
 main = do
   quickCheck (semigroupAssc :: TrivialAssc)
   quickCheck (semigroupAssc :: IdentityAssc (Sum Int))
+  quickCheck (semigroupAssc :: TwoAssc (Sum Int) (Product Int))
+  quickCheck (semigroupAssc :: ThreeAssc (Sum Int) (Product Int) [Int])
