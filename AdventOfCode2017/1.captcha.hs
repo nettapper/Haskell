e098dcdfcbf58c8924c17577677b93d1d8171712
Conner D <connerdunn7399@gmail.com>
2018-01-03 05:42:59 -0700

    Added README, stack exec script and hspec tests

--- AdventOfCode2017/1.captcha.hs
+++ AdventOfCode2017/1.captcha.hs
@@ -1,3 +1,6 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-9.6 script
+import Test.Hspec
 
 captcha :: String -> Integer
 captcha [] = 0
@@ -11,8 +14,16 @@ seqSum (a:b:others) = if a == b
                          else seqSum (b:others)
 
 main :: IO ()
-main = do
-  putStrLn $ show $ captcha "1122"
-  putStrLn $ show $ captcha "1111"
-  putStrLn $ show $ captcha "1234"
-  putStrLn $ show $ captcha "91212129"
+main = hspec $ do
+  describe "captcha" $ do
+    it "1122 should be 3" $ do
+      captcha "1122" `shouldBe` 3
+
+    it "1111 should be 4" $ do
+      captcha "1111" `shouldBe` 4
+
+    it "1234 should be 0" $ do
+      captcha "1234" `shouldBe` 0
+
+    it "91212129 should be 3" $ do
+      captcha "91212129" `shouldBe` 9
---
+++ AdventOfCode2017/README.md
@@ -0,0 +1,6 @@
+# AoC 2017
+http://adventofcode.com/2017
+
+# Running the code
+- First get `stack`
+- execute `stack filename.hs`
