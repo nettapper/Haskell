c360b2ca5e84ef44629c60121c377943c0f33f66
Conner D <connerdunn7399@gmail.com>
2018-01-04 00:02:17 -0700

    Found my mistake when parsing.. was mySplit :(

--- AdventOfCode2017/2.checksum.hs
+++ AdventOfCode2017/2.checksum.hs
@@ -8,12 +8,17 @@ main :: IO ()
 main = do
   lines <- myLoop []
   let spreadSheet = (map ((map read ) . mySplit (== ' ')) lines) :: [[Integer]]
+  putStrLn $ show $ calcChecksum spreadSheet
+  putStrLn "---"
+  runHspecTests
+
+calcChecksum :: [[Integer]] -> Integer
+calcChecksum spreadSheet = do
   let mins = map minimum spreadSheet
   let maxs = map maximum spreadSheet
   let sums = zipWith (-) maxs mins
   let checksum = sum sums
-  putStrLn $ show $ checksum
-
+  checksum
 
 myLoop :: [String] -> IO [String]
 myLoop lines = do
@@ -26,7 +31,25 @@ myLoop lines = do
 
 mySplit :: (Char -> Bool) -> String -> [String]
 mySplit pred str = go pred str [] []
-  where go _ [] _ list = list
+  where go _ [] [] list = list
+        go _ [] curr list = list ++ [curr]
         go pred (s:ss) curr list = if pred s
                                       then go pred ss [] (list ++ [curr])
                                       else go pred ss (curr ++ [s]) list
+
+runHspecTests :: IO ()
+runHspecTests =
+  hspec $ do
+    describe "calcChecksum" $ do
+      it "example given should be 18" $ do
+        let l = [[5,1,9,5]
+                ,[7,5,3]
+                ,[2,4,6,8]]
+        calcChecksum l `shouldBe` 18
+
+    describe "mySplit" $ do
+      it "should return a empty for an empty" $ do
+        mySplit (== ' ') [] `shouldBe` []
+      it "should 4 elems given \"5 1 9 5\"" $ do
+        let l = mySplit (== ' ') "5 1 9 5"
+        length l `shouldBe` 4
